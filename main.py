a133c60e2877daa42855e02aabf3aa7efffdaf01
AHpxMac <ahpx@yandex.com>
2024-12-17 14:42:43 +0800

    error

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import sys
 from config import read_config, write_default_config
-from login import *
+from login import login
 from reserve import *
 
 conf = read_config()
@@ -10,3 +10,7 @@ if conf == None:
     print('An empty config was generated as there\'s none, please fill out information.')
     write_default_config()
     exit()
+    
+ic = login(conf.user.username, conf.user.password)
+
+print(ic)
